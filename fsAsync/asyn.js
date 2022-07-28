d21d872715b2ab2ab413ce56ffdf2d32f6f400bd
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-07-28 16:38:35 +0530

    new folder

--- fsAsync/asyn.js
+++ fsAsync/asyn.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 
-fs.mkdir('trial',(err)=> {
-    console.log('create a folder');
+//fs.mkdir('trial',(err)=> {
+   // console.log('create a folder');
+//});
+
+fs.writeFile('./trial/bio.txt','Dumb yard!',(err) => {
+    console.log('file created');
 });
