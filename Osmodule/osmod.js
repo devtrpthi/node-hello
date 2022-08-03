149c5d482cc6669b5d8fb4c125f6ec7a8c3031a2
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-08-03 20:25:41 +0530

    total Memory

--- Osmodule/osmod.js
+++ Osmodule/osmod.js
@@ -2,5 +2,9 @@ const os = require('os');
 
 console.log(os.arch());
 
+
 const freememory = os.freemem();
 console.log(`${freememory/1024/1024/1024}`);
+
+const totalMemory =  os.totalmem();
+console.log(`${totalMemory/1024/1024/1024}`);
