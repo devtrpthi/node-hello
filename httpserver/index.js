8c36e3dfd74448fd7f87dcb73699ac40787a4a19
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-08-04 00:37:12 +0530

    routing

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req,res) => {
+    if(req.url === '/') {
     res.end('hello from the other side- Adele');
+    } else if(req.url === '/about') {
+        res.end('hello from the about');
+    } else if(req.url === '/contact'){
+        res.end('hello from the contact');
+    }
 });
 
 server.listen(8000,'127.0.0.1', () => {
