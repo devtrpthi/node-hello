d997d26604349170b05293b564948832a4f70f48
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-08-03 20:46:55 +0530

    parse

--- PathModule/path.js
+++ PathModule/path.js
@@ -2,4 +2,5 @@ const path  = require('path');
 
 console.log(path.dirname('/home/devesh/node-hello/PathModule/path.js'));
 console.log(path.extname('/home/devesh/node-hello/PathModule/path.js'));
-console.log(path.basename('/home/devesh/node-hello/PathModule/path.js'))
+console.log(path.basename('/home/devesh/node-hello/PathModule/path.js'));
+console.log(path.parse('/home/devesh/node-hello/PathModule/path.js'));
