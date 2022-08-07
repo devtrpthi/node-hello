8f23ae6ee47017b07a6275881ecc1b5147e8a573
devtrpthi <deveshkrishnatripathi@gmail.com>
2022-08-07 18:27:57 +0530

    replace-key

--- fnd-learn/index.mjs
+++ fnd-learn/index.mjs
@@ -1,3 +1,17 @@
-export const action = () => {
-    console.log("hello")
+import {readFile} from 'fs/promises'
+
+let template  = await readFile(new URL('template.html', import.meta.url),'utf-8')
+
+//console.log(template)
+
+const data = {
+    title: 'Random title',
+    body: 'Famous body'
 }
+
+
+for (const [k,v] of Object.entries(data)) {
+    template = template.replace(`{${k}}`, v)
+}
+
+console.log(template)
