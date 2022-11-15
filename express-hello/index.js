883cd17a6a77a4219a489904654b93dd38f0ed31
devesh <deveshkrishnatripathi@gmail.com>
2022-11-16 00:04:29 +0530

    commit

--- express-hello/index.js
+++ express-hello/index.js
@@ -4,7 +4,21 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req,res) => {
-  res.send("welcome to home page");
+  res.write("<h1>welcome to home page</h1>");
+  res.write("<h1>welcome to another same page</h1>");
+  res.send();
+})
+
+app.get('/about', (req,res) => {
+  res.send("welcome to about page");
+})
+
+app.get('/contact', (req,res) => {
+  res.send("welcome to contact  page");
+})
+
+app.get('/temp', (req,res) => {
+  res.send("welcome to temp page");
 })
 
 app.listen(port, () => {
