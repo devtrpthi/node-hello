fd7c2c97984c6414c78d7f02a5170a9093205e73
devesh <deveshkrishnatripathi@gmail.com>
2022-11-19 14:11:16 +0530

    insert:[one-doc]

--- learn-mongoo/src/app.js
+++ learn-mongoo/src/app.js
@@ -4,3 +4,35 @@ mongoose.connect('mongodb://127.0.0.1:27017/test1', {useNewUrlParser: true, useU
 .then( () => console.log('connection succes'))
 .catch( (err) => console.log(err));
 
+
+const listSchema = new mongoose.Schema({
+  name: String,
+  ctype: String,
+  videos: Number,
+  author: String,
+  active: Boolean,
+  date: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+const Playlist = new mongoose.model('Playlist', listSchema);
+
+const createDocument = async () => {
+  try{
+    const nodePlaylist = new Playlist({
+      name: 'nodejs',
+      ctype: 'backend',
+      videos: 10,
+      author: 'nobody',
+      active: false
+    })
+
+    const result = await nodePlaylist.save();
+    console.log(result);
+  }catch(err){
+    console.log(err);
+  }
+}
+ createDocument();
